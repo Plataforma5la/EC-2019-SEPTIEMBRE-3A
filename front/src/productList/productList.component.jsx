07d209718b2ad8997a712c769391cb45b216c5ec
munibarbieri <munibarbieri@gmail.com>
2019-11-06 14:46:31 -0300

    falta renderizar categorias

---
+++ front/src/productList/productList.component.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function ProductList({ products }) {
+  return (
+    <div>
+      {products.map(product => (
+        <div className="card" style={{ width: 200 }} key={product.productID}>
+          <Link className="thumbnail" to={`/${product.name}`}>
+            <img src={product.img1Url} className="card-img-top"></img>
+          </Link>
+
+          <div className="card-body">
+            <h5 className="card-title">{product.name}</h5>
+            <h5 className="card-title">{product.price}</h5>
+            <h5 className="card-title">{product.rating}</h5>
+            {/* <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+            <button>Agregar carrito</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default ProductList;
