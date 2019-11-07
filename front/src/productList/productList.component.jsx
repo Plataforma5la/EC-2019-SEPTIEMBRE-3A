de1b4c21c4d5456dc8a8bcecb502068ddb72430a
munibarbieri <munibarbieri@gmail.com>
2019-11-07 16:50:11 -0300

    asd

--- front/src/productList/productList.component.jsx
+++ front/src/productList/productList.component.jsx
@@ -3,13 +3,9 @@ import { Link } from "react-router-dom";
 
 function ProductList({ products, handleClick }) {
   return (
-<<<<<<< HEAD
-    <div className="card-deck">
-=======
-    <div id="page-content-wrapper">
->>>>>>> efaf3a8173a7ff7d0268fd7c8ac3aad6b5ff7f7b
+    <div className="main">
       {products.map(product => (
-        <div className="card" key={product.id}>
+        <div className="card " style={{ width: 200 }} key={product.id}>
           <Link
             className="thumbnail"
             to={`/product/${product.id}`}
@@ -17,23 +13,20 @@ function ProductList({ products, handleClick }) {
           >
             <img src={product.img1Url} className="card-img-top"></img>
           </Link>
+
           <div className="card-body">
             <h5 className="card-title">{product.name}</h5>
+
             <h5 className="card-title">{product.price}</h5>
+
             <h5 className="card-title">{product.ratingValue}</h5>
             {product.categories.map(category => (
               <i key={category.id} className="card-title">
                 #{category.name}
               </i>
             ))}
-            {/* <p className="card-text">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p> */}
-            {/* <p className="card-text">
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </p> */}
+
+            {/* <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
             <button>Agregar carrito</button>
           </div>
         </div>
@@ -43,29 +36,3 @@ function ProductList({ products, handleClick }) {
 }
 
 export default ProductList;
-
-// <div className="card" style={{ width: 200 }} key={product.id}>
-// <Link
-//   className="thumbnail"
-//   to={`/product/${product.id}`}
-//   onClick={() => handleClick(product.id)}
-// >
-//   <img src={product.img1Url} className="card-img-top"></img>
-// </Link>
-
-// <div className="card-body">
-//   <h5 className="card-title">{product.name}</h5>
-
-//   <h5 className="card-title">{product.price}</h5>
-
-//   <h5 className="card-title">{product.ratingValue}</h5>
-//   {product.categories.map(category => (
-//       <i key={category.id} className="card-title">#{category.name}</i>
-//   ))
-
-//   }
-
-//   {/* <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
-//   <button>Agregar carrito</button>
-// </div>
-// </div>
--- front/src/sidebar/sidebar.component.jsx
+++ front/src/sidebar/sidebar.component.jsx
@@ -1,31 +1,81 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default function ({ categories, FetchProductList, FilterbyCategory }) {
-
+export default function({ categories, FetchProductList, FilterbyCategory }) {
   return (
-
-
     <div class="sidenav">
       <section>
         <h4 className="text-muted">CATEGORIAS</h4>
-        <Link to={`/`} onClick={() => FetchProductList()} >VER TODAS</Link>
+        <Link to={`/`} onClick={() => FetchProductList()}>
+          VER TODAS
+        </Link>
         <ul className="list-unstyled">
-          {
-            categories.map(category => {
-              return (
-                <li key={category.id} className="playlist-item menu-item" onClick={() => FilterbyCategory(category.id)}>
-                  <Link to={`/`}>{category.name}</Link>
-                </li>
-              );
-            })
-          }
+          {categories.map(category => {
+            return (
+              <li
+                key={category.id}
+                className="playlist-item menu-item"
+                onClick={() => FilterbyCategory(category.id)}
+              >
+                <Link to={`/`}>{category.name}</Link>
+              </li>
+            );
+          })}
         </ul>
       </section>
     </div>
-
-
-
   );
 }
 
+{
+  /* <div class="wrapper">
+  
+    <nav id="sidebar">
+        <div class="sidebar-header">
+            <h3>Bootstrap Sidebar</h3>
+        </div>
+
+        <ul class="list-unstyled components">
+            <p>Dummy Heading</p>
+            <li class="active">
+                <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Home</a>
+                <ul class="collapse list-unstyled" id="homeSubmenu">
+                    <li>
+                        <a href="#">Home 1</a>
+                    </li>
+                    <li>
+                        <a href="#">Home 2</a>
+                    </li>
+                    <li>
+                        <a href="#">Home 3</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">About</a>
+            </li>
+            <li>
+                <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
+                <ul class="collapse list-unstyled" id="pageSubmenu">
+                    <li>
+                        <a href="#">Page 1</a>
+                    </li>
+                    <li>
+                        <a href="#">Page 2</a>
+                    </li>
+                    <li>
+                        <a href="#">Page 3</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Portfolio</a>
+            </li>
+            <li>
+                <a href="#">Contact</a>
+            </li>
+        </ul>
+    </nav>
+
+</div> */
+}
