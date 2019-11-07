9d07e2688ab03ae9e2fbb311f2716ca50a73f3d6
munibarbieri <munibarbieri@gmail.com>
2019-11-07 15:45:07 -0300

    bootstrap

--- front/src/productList/productList.component.jsx
+++ front/src/productList/productList.component.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 function ProductList({ products, handleClick }) {
   return (
-    <div>
+    <div className="card-deck">
       {products.map(product => (
-        <div className="card" style={{ width: 200 }} key={product.id}>
+        <div className="card" key={product.id}>
           <Link
             className="thumbnail"
             to={`/product/${product.id}`}
@@ -13,20 +13,23 @@ function ProductList({ products, handleClick }) {
           >
             <img src={product.img1Url} className="card-img-top"></img>
           </Link>
-
           <div className="card-body">
             <h5 className="card-title">{product.name}</h5>
-
             <h5 className="card-title">{product.price}</h5>
-
             <h5 className="card-title">{product.ratingValue}</h5>
             {product.categories.map(category => (
-                <i key={category.id} className="card-title">#{category.name}</i>
-            ))
-
-            }
-            
-            {/* <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+              <i key={category.id} className="card-title">
+                #{category.name}
+              </i>
+            ))}
+            {/* <p className="card-text">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p> */}
+            {/* <p className="card-text">
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </p> */}
             <button>Agregar carrito</button>
           </div>
         </div>
@@ -36,3 +39,29 @@ function ProductList({ products, handleClick }) {
 }
 
 export default ProductList;
+
+// <div className="card" style={{ width: 200 }} key={product.id}>
+// <Link
+//   className="thumbnail"
+//   to={`/product/${product.id}`}
+//   onClick={() => handleClick(product.id)}
+// >
+//   <img src={product.img1Url} className="card-img-top"></img>
+// </Link>
+
+// <div className="card-body">
+//   <h5 className="card-title">{product.name}</h5>
+
+//   <h5 className="card-title">{product.price}</h5>
+
+//   <h5 className="card-title">{product.ratingValue}</h5>
+//   {product.categories.map(category => (
+//       <i key={category.id} className="card-title">#{category.name}</i>
+//   ))
+
+//   }
+
+//   {/* <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+//   <button>Agregar carrito</button>
+// </div>
+// </div>
