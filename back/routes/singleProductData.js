d098af88a6a11fb42fbc27ea7843e86a0acc2941
aleaker <alejoakerfeld@gmail.com>
2019-11-07 17:45:39 -0300

    single product styled

--- back/routes/singleProductData.js
+++ back/routes/singleProductData.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const Product = require("../models/products.js");
+const Category = require ("../models/categories")
 
 router.get("/:productid", function(req, res) {
-  Product.findByPk(req.params.productid).then(productData =>
+  Product.findOne({where:{id:req.params.productid},include:[Category]}).then(productData =>{
     res.send(productData)
-  );
+  });
 });
 
 module.exports = router;
