5035ea9e10324588483f29a11632d96e04d2d5ef
aleaker <alejoakerfeld@gmail.com>
2019-11-08 15:02:16 -0300

    codigo zombie borrado

--- back/routes/productList.js
+++ back/routes/productList.js
@@ -17,12 +17,6 @@ router.get("/search/:text", function(req, res, next) {
       }
     
   }).then(productList => res.send(productList));
-  // Category.findOne({
-  //   where: {
-  //     id: req.params.id
-  //   },
-  //   include: [Product]
-  // }).then(productList => res.send(productList));
 });
 
 
@@ -30,12 +24,6 @@ router.get("/category/:id", function(req, res, next) {
   Product.findAll({
     include: [{ where: { id: req.params.id }, model: Category }]
   }).then(productList => res.send(productList));
-  // Category.findOne({
-  //   where: {
-  //     id: req.params.id
-  //   },
-  //   include: [Product]
-  // }).then(productList => res.send(productList));
 });
 
 router.get("/", function(req, res, next) {
--- front/src/productList/productList.component.jsx
+++ front/src/productList/productList.component.jsx
@@ -22,7 +22,6 @@ function ProductList({ products, handleClick }) {
               </i>
             ))}
             <br></br>
-            {/* <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
             <button type="button" className="btn btn-secondary">
               Deseo
             </button>
