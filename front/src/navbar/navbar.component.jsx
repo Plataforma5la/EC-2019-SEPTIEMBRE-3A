bc407792a82289d9e236d9ffd04537fc7e0f0e55
aleaker <alejoakerfeld@gmail.com>
2019-11-09 15:52:06 -0300

    navbar arreglada v2

--- front/src/navbar/navbar.component.jsx
+++ front/src/navbar/navbar.component.jsx
@@ -11,57 +11,48 @@ export default function MenuAppBar(props) {
   return (
     <div>
       <Navbar className="navbar">
-        
-          
-            <div className="logoContainer">
-              <Link to="/">
-                <Navbar.Brand
-                  id="titulo"
-                  onClick={() => props.FetchProductList()}
-                ><h2>
-
-                  E-Climax
-                </h2>
-                </Navbar.Brand>
-              </Link>
-            </div>
-            {props.user.username ? (
-              <div className="col-3">
-                <h4 className="textoNavbar">
-                  BienvenidX.X.X {props.user.username}
-                </h4>
-              </div>
-            ) : (
-              <div className="col-3">
-                <span></span>
-              </div>
-            )}
-            <div className="col-3">
-              <Form  onSubmit={props.handleSearch}>
-                <FormControl
-                  type="text"
-                  placeholder="Search"
-                  className="mr-sm-2"
-                  onChange={props.handleSearchtextChange}
-                />
-              </Form>
-            </div>
-            {props.user.username ? (
-              <div className="col-3">
-                <Button className="navButton" onClick={props.handleLogOut}>
-                  Logout
-                </Button>
-              </div>
-            ) : (
-              <div className="col-3 loginRegisterContainer">
-                <LoginContainer />
-                <span> </span>
-                <span> </span>
-                <RegisterContainer />
-              </div>
-            )}
-          
-   
+        <div className="logoContainer">
+          <Link to="/">
+            <Navbar.Brand id="titulo" onClick={() => props.FetchProductList()}>
+              <h2>E-Climax</h2>
+            </Navbar.Brand>
+          </Link>
+        </div>
+        {props.user.username ? (
+          <div className="col-3">
+            <h4 className="textoNavbar">
+              BienvenidX.X.X {props.user.username}
+            </h4>
+          </div>
+        ) : (
+          <div className="col-3">
+            <span></span>
+          </div>
+        )}
+        <div className="col-3">
+          <Form onSubmit={props.handleSearch}>
+            <FormControl
+              type="text"
+              placeholder="Search"
+              className="mr-sm-2"
+              onChange={props.handleSearchtextChange}
+            />
+          </Form>
+        </div>
+        {props.user.username ? (
+          <div className="col-3 navbarLogoutContainer">
+            <button className="navButton btn btn-drk btn-sm" onClick={props.handleLogOut}>
+              Logout
+            </button>
+          </div>
+        ) : (
+          <div className="col-3 loginRegisterContainer">
+            <LoginContainer />
+            <span> </span>
+            <span> </span>
+            <RegisterContainer />
+          </div>
+        )}
       </Navbar>
     </div>
   );
