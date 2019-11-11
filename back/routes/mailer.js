1507ec57861566c78e3bdbcf9f8d66bb5de77e31
Francisco Pegels <franpegels@10.100.0.30>
2019-11-11 18:44:26 -0300

    mail working

--- back/routes/mailer.js
+++ back/routes/mailer.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const nodeMailer = require('nodemailer');
 
 router.post("/", function(req, res) {
+    console.log(req.body.HTML_MESSAGE)
    
     let transporter = nodeMailer.createTransport({
         host: 'smtp.gmail.com',
@@ -17,7 +18,7 @@ router.post("/", function(req, res) {
         to: ['franpegels@gmail.com'/*, 'anyother@hotmail.com'*/],
         subject: 'COMPRA eClimax.com',
         text: 'Hello world?', // plain text body
-        html: '<div class="container cartProductsContainer"><div class="row cartSingleProductBox"><div class="col-3"><img class="cartProductPic " src="https://www.funfactory.com/fileadmin/360/DeluxeVIBES/MISS_BI/pink/1.png"></div><div class="col-3"><h4>Miss Bi</h4></div><div class="col-3"><p>$500</p></div><div class="col-3"><button>-</button><p>cant</p><button>+</button></div></div><div class="row cartSingleProductBox"><div class="col-3"><img class="cartProductPic " src="https://www.funfactory.com/fileadmin/360/CoupleTOYS/SHAREVIBE/dark-violet/1.png"></div><div class="col-3"><h4>ShareVibe</h4></div><div class="col-3"><p>$500</p></div><div class="col-3"><button>-</button><p>cant</p><button>+</button></div></div><div class="row cartSingleProductBox"><div class="col-3"><img class="cartProductPic " src="https://www.funfactory.com/fileadmin/360/StrongTOYS/STRONIC_G/pink/1.png"></div><div class="col-3"><h4>Stronic G</h4></div><div class="col-3"><p>$500</p></div><div class="col-3"><button>-</button><p>cant</p><button>+</button></div></div><div class="row cartSingleProductBox"><div class="col-3"><img class="cartProductPic " src="https://www.funfactory.com/fileadmin/360/DeluxeVIBES/VOLTA_-_BLACK_LINE/black/1.png"></div><div class="col-3"><h4>Volta</h4></div><div class="col-3"><p>$500</p></div><div class="col-3"><button>-</button><p>cant</p><button>+</button></div></div><button>Vaciar carrito</button></div>' // html body
+        html: req.body.HTML_MESSAGE // html body
     };
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
@@ -25,11 +26,11 @@ router.post("/", function(req, res) {
         }
         console.log('Message %s sent: %s', info.messageId, info.response);
     });
-    res.writeHead(301, { Location: 'index.html' });
-    res.end();
+    // res.writeHead(301, { Location: 'index.html' });
+      res.status(200);
+      res.json('Enviado con Exito!');
 
-    //   res.status(200);
-    //   res.json(categories);
+    
 
   });
   
--- front/src/store/actions/mailer.js
+++ front/src/store/actions/mailer.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 
-export const sendMail = function(mensajeHTML) {
+export const sendMail = function(direccion, cart) {
     return function(dispatch, getState) {
-      axios.put("/api/mailer", { mensajeHTML: mensajeHTML })
+      console.log("@@d", direccion, cart)
+
+let HTML_MESSAGE = '<div><b>Gracias por tu compra!</b> <br>'
+for (let i=0; i<cart.length; i++){
+  HTML_MESSAGE += `(${cart[i].cart_product.count}) ${cart[i].name} <br>`
+}
+HTML_MESSAGE += `<b>Te lo enviamos a ${direccion}</b></div>`
+console.log(HTML_MESSAGE)
+      axios.post("/api/mailer", { HTML_MESSAGE: HTML_MESSAGE }).then(()=> alert("Gracias por tu compra! Te hemos enviado los detalles a tu casilla de correo"))
     };
   };
