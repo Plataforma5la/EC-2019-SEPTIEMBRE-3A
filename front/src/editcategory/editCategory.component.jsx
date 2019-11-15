2ecdd7894cdaea9c95ce20ed941799a7f916f996
munibarbieri <munibarbieri@gmail.com>
2019-11-15 11:01:42 -0300

    Edit Category table done

--- front/src/editcategory/editCategory.component.jsx
+++ front/src/editcategory/editCategory.component.jsx
@@ -1,9 +1,101 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Table } from "react-bootstrap";
 
 function EditCategoryComponent({ categories, handleChange, handleSubmit }) {
   return (
-    <div className="categoryList">
+    <div className="editCategoryNames">
+      <h3>Edit category names</h3>
+
+      <Table striped bordered hover>
+        <thead className="columCategoryEditTable">
+          <tr>
+            <th>Actual category name</th>
+            <th>New category name</th>
+            <th>Change category name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {categories.map(cat => (
+            <tr key={cat.id}>
+              <td className="categoriesNameColor">{cat.name}</td>
+              <td>
+                {" "}
+                <input
+                  placeholder="change category name"
+                  name="name"
+                  type="text"
+                  onChange={e => handleChange(e)}
+                ></input>
+              </td>
+              <td>
+                {" "}
+                <Link to={"/"}>
+                  <button
+                    type="button"
+                    className="btn btn-success"
+                    onClick={event => {
+                      event.id = cat.id;
+                      handleSubmit(event);
+                    }}
+                  >
+                    Edit category name
+                  </button>
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
+}
+
+export default EditCategoryComponent;
+
+{
+  /* <div>
+          <h3>Edit category names</h3>
+
+<Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Actual category name</th>
+            <th>New category name</th>
+            <th>Change category name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {categories.map(cat=> (
+            <tr key={cat.id}></tr>
+            <td>{cat.name}</td>
+                <td> <input
+              placeholder="change category name"
+              name="name"
+              type="text"
+              onChange={e => handleChange(e)}
+            ></input></td>
+            <td> <Link to={"/"}>
+              <button
+                type="button"
+                className="btn btn-success"
+                onClick={event => {
+                  event.id = cat.id;
+                  handleSubmit(event);
+                }}
+              >
+                Edit category name
+              </button>
+            </Link></td>
+            </tr>
+          ))}
+        </tbody>
+        </div> */
+}
+
+{
+  /* <div className="categoryList">
       <ul className="list-group">
         {categories.map(cat => (
           <li className="list-group-item" key={cat.id}>
@@ -31,8 +123,5 @@ function EditCategoryComponent({ categories, handleChange, handleSubmit }) {
           </li>
         ))}
       </ul>
-    </div>
-  );
+    </div> */
 }
-
-export default EditCategoryComponent;
